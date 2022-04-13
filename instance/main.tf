286948aefe008ebb0cc782a9c138c77b93798796
Mohammad Yahya Akhlaqi <91276692+myakhlaqi@users.noreply.github.com>
2022-04-14 00:46:51 +0800

    Update main.tf

--- instance/main.tf
+++ instance/main.tf
@@ -16,7 +16,7 @@ resource "google_compute_instance" "vm_instance" {
   }
   network_interface {
     network = "${var.instance_network}"
-    access_config {
+    access_config { 
       # Allocate a one-to-one NAT IP to the instance
     }
   }
